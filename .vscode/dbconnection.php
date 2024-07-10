22cb0d7dbded179b737f8d11cd0250eb2d772528
ivyanneh <167083813+ivyanneh@users.noreply.github.com>
2024-07-11 00:22:52 +0300

    Update dbconnection.php

--- .vscode/dbconnection.php
+++ .vscode/dbconnection.php
@@ -2,17 +2,13 @@
 $servername = "localhost"; // Server name, usually 'localhost' for local development
 $username = "root"; // Database username, typically 'root' for local development
 $password = ""; // Database password, often left empty for local development
-$dbname = "Sweet delites"; // Database name to connect to
-
+$dbname = "sweet_delites"; // Database name to connect to
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = mysqli_connect($servername, $username, $password,$dbname);
 
 // Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
 }
-
-// Uncomment the line below if you want to output a success message when connected successfully
-// echo "Connected successfully";
-
+echo "Connected successfully";
 ?>
