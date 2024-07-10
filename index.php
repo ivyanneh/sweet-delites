8969862f6559efef8a5527dfbfc69249aba2ab01
ivyanneh <167083813+ivyanneh@users.noreply.github.com>
2024-07-11 00:52:46 +0300

    Update index.php

--- index.php
+++ index.php
@@ -104,14 +104,10 @@
 <body>
   <header class="header">
  
-  <?php
-$imagePath = "C:\Users\ivymu\Desktop\images\logo.png";
-$altText = "Sweet Delights Bakery Logo";
-echo "<img src='$imagePath' alt='$altText'>";
-?>
+  <img src= "Images.php/logo.png" alt="logo" style="width:10%">
 
    
-    <h1>Sweet Delights Bakery</h1>
+    <h1>Sweet Delites Bakery</h1>
   </header>
   
 
@@ -146,25 +142,25 @@ echo "<img src='$imagePath' alt='$altText'>";
       
     <div class="polaroid">
     
-      <img src="C:\Users\ivymu\OneDrive\Documents\GitHub\sweet-delites\Images.php\fron view.jpeg"alt="front view" style="width:100%">
+      <img src="Images.php/shop exterior.jpeg"alt="front view" style="width:100%">
       <div class="container">
       <p>front view</p>
       </div>
     </div>
     <div class="polaroid">
-      <img src="C:\Users\ivymu\OneDrive\Documents\GitHub\sweet-delites\Images.php\interior.jpeg" alt="Interior" style="width:100%">
+      <img src="Images.php/interior.jpeg" alt="Interior" style="width:100%">
       <div class="container">
       <p>Interior</p>
       </div>
     </div>
     <div class="polaroid">
-      <img src="C:\Users\ivymu\OneDrive\Documents\GitHub\sweet-delites\Images.php\back view.jpeg" alt="Back view" style="width:100%">
+      <img src="Images.php/back view.jpeg" alt="Back view" style="width:100%">
       <div class="container">
       <p>Back view</p>
       </div>
     </div>
       <div class="polaroid">
-      <img src="C:\Users\ivymu\OneDrive\Documents\GitHub\sweet-delites\Images.php\side view.jpeg" alt="Side view" style="width:100%">
+      <img src="Images.php/side view.jpeg" alt="Side view" style="width:100%">
       <div class="container">
       <p>Side view</p>
       </div>
