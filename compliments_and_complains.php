d80f75869263115cab85302e4fce083b0fe83a39
ivyanneh <167083813+ivyanneh@users.noreply.github.com>
2024-07-10 15:34:31 +0300

    Create compliments_and_complains.php

---
+++ compliments_and_complains.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Compliments and complains</title>
+</head>
+<body style="background-color: lightgrey;">
+
+    <title>compliments and complains</title>
+</head>
+<body style="background-color: lightgrey;">
+    <a href= "order.html"> Order now </a>
+<div>
+
+
+    <h2><i>Sweet Delicacies</i></h2>
+    <img src="C:\Users\ivymu\Desktop\Sweet Delicacies\download.jpeg" alt="Sweet Delicacies" width="800", height="500",border="3"/>
+    
+    <h1>  WELCOME TO SWEET DELITES BAKERY SHOP</h1>
+    <p>We work tirelessly to ensure we meet our customers needs. 
+        All compliments and comments on the workers and services are highly appreciated.
+        There is also room for improvent so feel free to write a complaint below so that we are able to serve you better.</p>
+
+
+       
+   </div>  
+</body>
+</html>
+
+
+ 
